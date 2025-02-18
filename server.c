9cc178d4fd31905d43e10adf9a8c6ef8dacf80cf
wen2ms <1819509111@qq.com>
2025-02-18 19:55:43 +0800

    Feat: implement a multithreaded concurrent server

--- server.c
+++ server.c
@@ -1,6 +1,17 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
+struct SocketInfo {
+    struct sockaddr_in client_address;
+    int client_file_descriptor;
+};
+
+struct SocketInfo socket_infos[512];
+
+void* worker(void* arg);
 
 int main() {
     int file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -30,39 +41,74 @@ int main() {
         return -1;
     }
 
-    struct sockaddr_in client_address;
-    unsigned int client_address_len = sizeof(client_address);
+    int socket_infos_max = sizeof(socket_infos) / sizeof(socket_infos[0]);
 
-    int client_file_descriptor = accept(file_descriptor, (struct sockaddr*)&client_address, &client_address_len);
+    for (int i = 0; i <socket_infos_max; ++i) {
+        bzero(&socket_infos[i], sizeof(socket_infos[i]));
 
-    if (client_file_descriptor == -1) {
-        perror("accept");
-        return -1;
+        socket_infos[i].client_file_descriptor = -1;
     }
 
-    char ip[32];
-    printf("Client IP: %s, Port: %d\n", inet_ntop(AF_INET, &client_address.sin_addr.s_addr, ip, sizeof(ip)),
-           ntohs(client_address.sin_port));
+    unsigned int client_address_len = sizeof(struct sockaddr);
+
+    while (1) {
+        struct SocketInfo* p_info;
+
+        for (int i = 0; i < socket_infos_max; ++i) {
+            if (socket_infos[i].client_file_descriptor == -1) {
+                p_info = &socket_infos[i];
+                break;
+            }
+        }
+
+        int client_file_descriptor = accept(file_descriptor, (struct sockaddr*)&p_info->client_address, &client_address_len);
+        p_info->client_file_descriptor = client_file_descriptor;
+
+        if (client_file_descriptor == -1) {
+            perror("accept");
+            break;
+        }
+
+        pthread_t tid;
+
+        pthread_create(&tid, NULL, worker, p_info);
         
+        pthread_detach(tid);
+    }
+
+    close(file_descriptor);
+
+    return 0;
+}
+
+void* worker(void* arg) {
+    printf("worker id=%p...\n", pthread_self());
+
+    struct SocketInfo* p_info = (struct SocketInfo*)arg;
+
+    char ip[32];
+    printf("Client IP: %s, Port: %d\n", inet_ntop(AF_INET, &p_info->client_address.sin_addr.s_addr, ip, sizeof(ip)),
+           ntohs(p_info->client_address.sin_port));
+
     while (1) {
         char buffer[1024];
-        int receive_data_len = recv(client_file_descriptor, buffer, sizeof(buffer), 0);
+        int receive_data_len = recv(p_info->client_file_descriptor, buffer, sizeof(buffer), 0);
 
         if (receive_data_len > 0) {
             printf("client say: %s\n", buffer);
 
-            send(client_file_descriptor, buffer, receive_data_len, 0);
+            send(p_info->client_file_descriptor, buffer, receive_data_len, 0);
         } else if (receive_data_len == 0) {
             printf("client disconnected...\n");
             break;
         } else {
             perror("recv");
-            return -1;
+            break;
         }
     }
 
-    close(file_descriptor);
-    close(client_file_descriptor);
+    close(p_info->client_file_descriptor);
+    p_info->client_file_descriptor = -1;
 
-    return 0;
+    return NULL;
 }
